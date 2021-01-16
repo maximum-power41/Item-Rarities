b2a0bb5b58d572462565c1e0454e2bd6919a1674
BuildTools <unconfigured@null.spigotmc.org>
2021-01-15 20:29:47 -0500

    Added files for project. Updated gradle.properties to have up to date library versions and information. Deleted fabric's example code and added ItemRarities.java.

---
+++ src/main/java/me/giulianomaxwell/mods/ItemRarities.java
@@ -0,0 +1,11 @@
+package me.giulianomaxwell.mods;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ItemRarities implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
