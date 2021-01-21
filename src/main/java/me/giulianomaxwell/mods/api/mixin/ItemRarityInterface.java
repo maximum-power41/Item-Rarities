fa752e47888764b77eaca1608a7fbb2aa477e833
BuildTools <unconfigured@null.spigotmc.org>
2021-01-20 22:28:20 -0500

    Created ItemRarityInterface and ItemRarityMixin class. Implemented ItemRarityInterface in ItemRarityMixin.

---
+++ src/main/java/me/giulianomaxwell/mods/api/mixin/ItemRarityInterface.java
@@ -0,0 +1,10 @@
+package me.giulianomaxwell.mods.api.mixin;
+
+import me.giulianomaxwell.mods.api.Rarity;
+
+public interface ItemRarityInterface {
+
+    Rarity getRarity();
+
+    void setRarity(Rarity rarity);
+}
