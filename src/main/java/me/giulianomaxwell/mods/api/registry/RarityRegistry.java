c42e2137ce6451c8e303d5817a7a5a4c6e423056
BuildTools <unconfigured@null.spigotmc.org>
2021-01-20 21:24:59 -0500

    Implemented RegistryInterface in RarityRegistry.java

--- src/main/java/me/giulianomaxwell/mods/api/registry/RarityRegistry.java
+++ src/main/java/me/giulianomaxwell/mods/api/registry/RarityRegistry.java
@@ -1,14 +1,15 @@
 package me.giulianomaxwell.mods.api.registry;
 
+import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
+import me.giulianomaxwell.mods.api.Rarity;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.Rarity;
 
-import java.util.Map;
+import java.util.Set;
 
-public final class RarityRegistry {
+public final class RarityRegistry implements RegistryInterface<Rarity> {
 
-    private final Map<Identifier, Rarity> rarityMap;
+    private final BiMap<Identifier, Rarity> rarityMap;
     private static final RarityRegistry RARITY_REGISTRY_INSTANCE = new RarityRegistry();
 
     private RarityRegistry() {
@@ -17,5 +18,30 @@ public final class RarityRegistry {
 
     public static RarityRegistry getInstance() {
         return RARITY_REGISTRY_INSTANCE;
+    }
+
+    @Override
+    public void register(Identifier identifier, Rarity value) {
+        rarityMap.put(identifier, value);
+    }
+
+    @Override
+    public Rarity getValue(Identifier identifier) {
+        return rarityMap.get(identifier);
+    }
+
+    @Override
+    public Identifier getIdentifier(Rarity value) {
+        return rarityMap.inverse().get(value);
+    }
+
+    @Override
+    public Set<Identifier> getIdentifiers() {
+        return rarityMap.keySet();
+    }
+
+    @Override
+    public Set<Rarity> getValues() {
+        return rarityMap.values();
     }
 }
