e8fb155c08b3683361a68d342304e286fd602196
BuildTools <unconfigured@null.spigotmc.org>
2021-01-20 21:27:40 -0500

    Removed Identifier from Rarity.java

--- src/main/java/me/giulianomaxwell/mods/api/Rarity.java
+++ src/main/java/me/giulianomaxwell/mods/api/Rarity.java
@@ -1,19 +1,11 @@
 package me.giulianomaxwell.mods.api;
 
-import net.minecraft.util.Identifier;
-
 public abstract class Rarity {
 
-    private final Identifier identifier;
     private final String displayName;
 
-    public Rarity(Identifier identifier, String displayName) {
-        this.identifier = identifier;
+    public Rarity(String displayName) {
         this.displayName = displayName;
-    }
-
-    public Identifier getIdentifier() {
-        return identifier;
     }
 
     public String getDisplayName() {
